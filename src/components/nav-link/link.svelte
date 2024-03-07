505eeb132025c12342980cf177c5bd46af2b4b2e
Ram Goel <rgoel766@gmail.com>
2024-03-07 22:13:48 +0530

    chore: hover styles

--- src/components/nav-link/link.svelte
+++ src/components/nav-link/link.svelte
@@ -2,7 +2,7 @@
 	import type { LinkConfigType } from "../header/header";
     export let linkObject:LinkConfigType;
 </script>
-<div class="flex  items-center justify-center px-2 mx-4">
+<div class="flex border-b-[2px] border-b-white hover:border-b-gray-400 transition-all cursor-pointer py-1 items-center justify-center px-2 mx-3">
    <img src={linkObject.icon} alt="header-link-logo" class="mr-2 hover:text-black" width="16" height="16" />
-   <p class=" text-gray-600 text-[14px] hover:text-black">{linkObject.name}</p>
+   <p class=" text-gray-600 text-[14px] ">{linkObject.name}</p>
 </div>
--- src/components/nav-user/user.svelte
+++ src/components/nav-user/user.svelte
@@ -3,7 +3,7 @@
     export let user:any;
 </script>
 <div class="flex  items-center justify-center px-2 mx-4">
-   <img src={user.image} alt="header-link-logo" class="mr-2 hover:text-black" width="40" height="40" />
+   <img src={user.image} alt="header-link-logo" class="mr-2 hover:opacity-70 cursor-pointer" width="40" height="40" />
    <p class=" text-gray-600 hover:text-black">{user.name}</p>
    <img src={ChevronDown} alt="header-link-logo" width="16" height="16" class="mt-1 mx-2" />
 </div>
